5c7ca078382e53961d4b2248a4f07596548e5762
WilliamC Eustis <willceusits@gmail.com>
2020-12-10 21:18:43 -0500

    edits

--- vitss.py
+++ vitss.py
@@ -4,17 +4,17 @@ from vit_pytorch import ViT
 v = ViT(
     image_size = 320,
     patch_size = 16,
-    num_classes = 1000,
+    num_classes = 250,
     dim = 1024,
-    depth = 6,
-    heads = 8,
-    mlp_dim = 2048,
+    depth = 2,
+    heads = 6,
+    mlp_dim = 512,
     dropout = 0.1,
     emb_dropout = 0.1
 )
 
-img = torch.randn(1, 3, 256, 256)
-mask = torch.ones(1, 8, 8).bool() # optional mask, designating which patch to attend to
+img = torch.randn(1, 3, 320, 320)
+mask = torch.ones(1, 20, 20).bool() # optional mask, designating which patch to attend to
 
 preds = v(img, mask = mask) # (1, 1000)
 print(preds)
